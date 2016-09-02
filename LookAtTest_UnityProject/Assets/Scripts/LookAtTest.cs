0920251aa6e79b33ce9920c97134adcd49873263
Tom Trahey <tomtrahey@yahoo.com>
2016-09-01 22:35:04 -0400

    initial commit

---
+++ LookAtTest_UnityProject/Assets/Scripts/LookAtTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtTest : MonoBehaviour
+{
+    [Header("RIG JOINTS")]
+    public Transform j_rightEye;
+    public Transform j_leftEye;
+
+    [Space(5)]
+    public Transform j_head;
+    public Transform j_neck;
+
+    [Space(5)]
+    public Transform j_chest;
+    public Transform j_spine01;
+    public Transform j_spine02;
+
+
+    public void Awake()
+    {
+        // get rig's initial "look forward" rotations
+    }
+}
