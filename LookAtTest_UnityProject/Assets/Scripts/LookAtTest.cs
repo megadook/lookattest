6bf663368daf8f9521a7df81b3b418e7f716f003
megadook <tomtrahey@yahoo.com>
2016-09-03 17:35:50 -0400

    yea

--- LookAtTest_UnityProject/Assets/Scripts/LookAtTest.cs
+++ LookAtTest_UnityProject/Assets/Scripts/LookAtTest.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class LookAtTest : MonoBehaviour
 {
     [System.Serializable]
@@ -8,42 +9,63 @@ public class LookAtTest : MonoBehaviour
     {
         public Transform joint;
         public Vector3 baseRot;
+
+        public float xOffset, yOffset, zOffset;
+
+        // keep track of current used lookAtOffset, so when target is behind,
+        // move back towards no offset at all
+
+        public void RecordBaseRot()
+        {
+            if (joint != null)
+            {
+                baseRot = joint.localRotation.eulerAngles;
+            }
+        }
     }
 
+    public Transform character;
+
+    public float lookAtWeight; // set in inspector
+    private float _lookAtWeight;
+
     [Header("RIG JOINTS")]
-    public JointData j_rightEye;
-    public JointData j_leftEye;
+    public JointData r_eye;
+    public JointData l_eye;
 
     [Space(5)]
-    public JointData j_head;
-    public JointData j_neck;
+    public JointData head;
+    public JointData neck;
 
     [Space(5)]
-    public JointData j_chest;
-    public JointData j_spine01;
-    public JointData j_spine02;
+    public JointData chest;
+    public JointData[] spine;
 
 
     public void Awake()
     {
         // get rig's initial "look forward" rotations
-        //j_rightEye.baseRot = j_rightEye.joint.rotation.eulerAngles;
-        //j_leftEye.baseRot = j_leftEye.joint.rotation.eulerAngles;
-        j_head.baseRot = j_head.joint.localRotation.eulerAngles;
-        j_neck.baseRot = j_neck.joint.localRotation.eulerAngles;
-        //j_chest.baseRot = j_chest.joint.rotation.eulerAngles;
-        //j_spine01.baseRot = j_spine01.joint.rotation.eulerAngles;
-        //j_spine02.baseRot = j_spine02.joint.rotation.eulerAngles;
+        head.RecordBaseRot();
+        neck.RecordBaseRot();
+        chest.RecordBaseRot();
+
+        for (int i = 0; i < spine.Length; i++)
+        {
+            spine[i].RecordBaseRot();
+        }
     }
-    public float xOffset, yOffset, zOffset;
+
     public void LateUpdate()
     {
-        Vector3 lookDir = (transform.position - j_head.joint.position).normalized;
+        Vector3 lookDir = -character.InverseTransformDirection(transform.position - head.joint.position).normalized;
         float tempX, tempY, tempZ;
 
-        tempX = Mathf.Atan2(lookDir.z, lookDir.x) * Mathf.Rad2Deg + xOffset;
-        tempY = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + yOffset;
-        tempZ = Mathf.Atan2(-lookDir.y, lookDir.z) * Mathf.Rad2Deg + zOffset;
-        j_head.joint.localRotation = Quaternion.Euler(j_head.baseRot + new Vector3(tempX, 0, tempZ));
+        Debug.Log("lookDir = " + lookDir);
+
+        tempX = Mathf.Atan2(lookDir.z, lookDir.x) * Mathf.Rad2Deg + head.xOffset;
+        tempY = (Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + head.yOffset);
+        tempZ = (Mathf.Atan2(lookDir.y, -Mathf.Abs(lookDir.z)) * Mathf.Rad2Deg) + head.zOffset;
+
+        head.joint.localRotation = Quaternion.Euler(head.joint.localRotation.eulerAngles + new Vector3(tempX, 0, tempZ));
     }
 }
